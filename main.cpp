937e453025bfb291a6d3e1b967a7bf3afc6449a6
III <42315061+SaOxXx@users.noreply.github.com>
2022-04-16 21:51:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <map>
+#include <list>
+#include "multimap.h"
+using namespace std;
+
+int main()
+{	Multimap a;
+	Multimap b;
+	Multimap c;
+	Multimap d;
+	cout << "Input list:" << endl;
+	a.insert("a",1);
+	a.insert("c", 2);
+	a.insert("b", 3);
+	a.insert("d", 4);
+	a.insert("e", 1);
+	a.insert("f", 1);
+	a.insert("f", 222);
+	a.insert("f", 3);
+	a.insert("a", 4);
+	a.insert("s", 1);
+	a.insert("c", 2);
+	a.insert("b", 3);
+	a.erase("b");
+	b.insert("a", 4);
+	b.insert("a", 90);
+	b.insert("c", 1);
+	b.insert("c", 2);
+	b.insert("z", 88);
+	b.insert("z", 76);
+	c = a+b;
+	cout << c << endl;
+	d += c;
+	cout << d << endl;
+	//cout << a << endl;
+	//cout << b << endl;
+	//a += c;
+	//cout << a;
+
+	return 0;
+}
---
+++ multimap.h
@@ -0,0 +1,132 @@
+﻿#pragma once
+#include <map>
+#include <list>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+class Multimap {
+private:
+	map<const string, list<int>> m;
+public:
+	Multimap() {}
+	void insert(const string& key, int value){
+		if (m.empty() == 1) { 
+			list<int> l;
+			l.push_back(value);
+			
+			m[key] = l;
+		}
+		else {
+			map<const string, list<int>>::iterator p;
+ 			p = m.find(key);
+			if (p != m.end()) {
+				p->second.push_back(value);
+			}
+			else {
+				list<int>l;
+				l.push_back(value);
+				m.insert(std::pair<const string&,list<int>&>(key, l));
+			}
+		}  
+	}
+	const list<int>& find(string& key) {
+		map<const string, list<int>>::iterator p;
+		p = m.find(key);
+		if (p != m.end()) {
+			return p->second;
+		}
+		else {
+			list<int> l;
+			m.insert(std::pair<const string&, list<int>&>(key, l));
+			return l;
+		}
+	}
+	void erase(const string& key) {
+		map<const string, list<int>>::iterator p;
+		p = m.find(key);
+		if (p != m.end()) {
+			m.erase(p);
+		}
+	}
+
+	map<const string, list<int>>& return_map() {
+		return m;
+	}
+	
+	Multimap operator +(Multimap& dop) {
+		  Multimap result;
+		map<const string, list<int>>& dop_m = dop.return_map();
+		
+
+		for (map<const string, list<int>>::iterator p = m.begin(); p != m.end(); p++) {
+			for (list<int>::iterator it = p->second.begin(); it != p->second.end(); it++) {
+				result.insert(p->first, *it);
+			}
+		}
+
+		for (map<const string, list<int>>::iterator p = dop.m.begin(); p != dop.m.end(); p++) {
+			for (list<int>::iterator it = p->second.begin(); it != p->second.end(); it++) {
+				result.insert(p->first, *it);
+			}
+		}
+		return result;
+	}
+
+
+
+	Multimap operator +=(Multimap& other) {
+		Multimap result;
+		map<const string, list<int>>& dop_m = other.return_map();
+		map<const string, list<int>>& result_m = result.return_map();
+
+		for (map<const string, list<int>>::iterator p = m.begin(); p != m.end(); p++) {
+			for (list<int>::iterator it = p->second.begin(); it != p->second.end(); it++) {
+				result.insert(p->first, *it);
+			}
+		}
+
+		for (map<const string, list<int>>::iterator p = other.m.begin(); p != other.m.end(); p++) {
+			for (list<int>::iterator it = p->second.begin(); it != p->second.end(); it++) {
+				result.insert(p->first, *it);
+			}
+		}
+		*this = result;
+		return *this; 
+	}
+
+
+
+
+
+
+	Multimap& operator=(Multimap A) {
+
+		map<const string, list<int>>& dop_m = A.return_map();
+		m = dop_m;
+		return *this;
+	}
+
+};
+ostream& operator<<(ostream& out, Multimap& a) {
+	map<const string, list<int>>& m = a.return_map();
+	map<const string, list<int>>::iterator p;
+
+	p = m.begin();
+	while (p != m.end()) {
+		out << p->first << ":";
+		for (list<int>::iterator i = p->second.begin(); i != p->second.end(); i++) {
+			out << *i;
+			out << "    ";
+		}
+		out << "\n";
+		p++;
+	}
+	return out;
+
+	
+
+
+
+}
